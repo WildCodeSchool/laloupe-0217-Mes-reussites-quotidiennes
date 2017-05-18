f653d67694ac59c88b35af0a203eeff64b2adc65
Philippe Pinceloup <ph.pinceloup@gmail.com>
2017-05-18 14:02:47 +0200

    Reussites de la session page updated

---
+++ app/js/controllers/reussitesController.js
@@ -0,0 +1,31 @@
+angular.module('app')
+    .controller('ReussitesController', function($scope, CurrentUser, PostService) {
+      $scope.user = CurrentUser.user();
+      function load() {
+        PostService.getAll().then(function(res) {
+          $scope.posts = res.data;
+        });
+      }
+
+      $(function(){
+       $("textarea").prop('required',true);
+});
+
+      load();
+      $scope.test = 'test';
+      $scope.newPost = '';
+
+      $scope.sendPost = function() {
+        PostService.create({
+          content: $scope.newPost
+        }).then(function(res) {
+          load();
+          $scope.newPost = '';
+        });
+      };
+      $scope.removePost = function(id) {
+        PostService.delete(id).then(function() {
+          load();
+        });
+      };
+});
