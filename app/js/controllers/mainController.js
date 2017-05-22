422ae6b690bd316b1b1dec28ac79f70264f5e08c
Angelique Clery <angeliqueclery@yahoo.fr>
2017-05-22 14:14:06 +0200

    fixing bug

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,9 +1,18 @@
 angular.module('app')
-    .controller('MainController', function($scope, CurrentUser) {
+    .controller('MainController', function($scope, CurrentUser, BadgeService, $state) {
 
       $scope.user = CurrentUser.user();
+
+      //add color emoji on click
       $scope.changeColor = function(id){
         $scope.color = id;
       };
 
-    });
+      //initialisation demand badges
+      $scope.newDemand = "";
+      $scope.sendDemand = function() {
+        BadgeService.create().then(function(res){
+          $state.go('user.badger');
+      });
+    };
+});
